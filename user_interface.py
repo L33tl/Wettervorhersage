26645d359bbc41b41cec8c8d69a03929e65f0163
JaddaHub <56644672+JaddaHub@users.noreply.github.com>
2021-11-01 15:26:37 +0500

    start

--- user_interface.py
+++ user_interface.py
@@ -1,8 +1,17 @@
 import sys
+
+from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow
-from PyQt5 import QtCore, QtGui, QtWidgets
 
 
-# class MainWindow(QMainWindow, Ui_MainWindow):
-#     def __init__(self):
-#         pass
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('main_window_ui.ui', self)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MainWindow()
+    ex.show()
+    sys.exit(app.exec_())
