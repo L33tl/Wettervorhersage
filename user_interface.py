9822143924d3491a35a5fe2948ad89efa62a6d57
JaddaHub <56644672+JaddaHub@users.noreply.github.com>
2021-11-11 14:30:28 +0500

    Getting weather from api

--- user_interface.py
+++ user_interface.py
@@ -1,17 +1,90 @@
 import sys
 
+import geocoder
 from PyQt5 import uic
-from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow
+from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QApplication, QMainWindow, QGridLayout, QWidget, QAction, qApp
+from main_window_ui import Ui_MainWindow
+from weather import WeatherWorker
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.change_city_form = ChangeCityForm(self)
         uic.loadUi('main_window_ui.ui', self)
+        # self.setupUi(self)
+        self.buttons_group = {
+            'today': self.Btn_today, 'days': self.Btn_byDays, 'hours': self.Btn_byHours
+            }
+        for btn in self.buttons_group.values():
+            btn.clicked.connect(self.group_button_click)
+        # self.change_city_btn.clicked.connect(self.change_city)
+
+        self.change_city_btn.triggered.connect(self.change_city)
+        self.weather = WeatherWorker()
+
+    def change_city(self):
+        self.change_city_form.show()
+
+    def group_button_click(self):
+        if self.buttons_group['today'] == self.sender():
+            self.__move_to_layout(self.widget_today)
+        elif self.buttons_group['days'] == self.sender():
+            self.__move_to_layout(self.widget_days)
+        elif self.buttons_group['hours'] == self.sender():
+            self.__move_to_layout(self.widget_hours)
+
+    def __move_to_layout(self, to_layout: QWidget):
+        self.widget_today.hide()
+        self.widget_days.hide()
+        self.widget_hours.hide()
+        # self.update_layout(to_layout)
+        to_layout.show()
+
+    def load_layout_today(self, layout):
+        pass
+
+    def load_layout_by_days(self, layout):
+        pass
+
+    def load_layout_by_hours(self, layout):
+        pass
+
+    def get_weather(self):
+        has_connection = self.weather.has_connected()
+        if self.today_layout.isEnabled():
+            self.weather.weather('today', has_connection)
+        elif self.days_layout.isEnabled():
+            self.weather.weather('days', has_connection)
+        else:
+            self.weather.weather('hours', has_connection)
+
+
+class ChangeCityForm(QWidget):
+    def __init__(self, first_form: MainWindow):
+        super().__init__()
+        uic.loadUi('change_city_UI.ui', self)
+        self.ok_btn.clicked.connect(self.ok)
+        self.cancel_btn.clicked.connect(self.cancel)
+        self.not_found_error_label.setText('')
+        self.first_form = first_form
+
+    def ok(self):
+        city = self.city_edit.text().strip()
+        answer = WeatherWorker.change_city(self.first_form.weather, city)
+        if answer:
+            self.close()
+        else:
+            self.not_found_error_label.setText('Город не найден')
+
+    def cancel(self):
+        self.city_edit.setText('')
+        self.close()
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = MainWindow()
     ex.show()
-    sys.exit(app.exec_())
+    sys.exit(app.exec())
--- weather.py
+++ weather.py
@@ -1,24 +1,79 @@
+import pyowm.weatherapi25.observation
 from pyowm import OWM
+import geocoder
+from pyowm.commons.exceptions import NotFoundError
+from pyowm.weatherapi25.weather import Weather
+
 from config import API_key
 from pyowm.utils.config import get_default_config
+from db_Worker import DBWorker
+
+import socket
 
 
-class Weather:
+class WeatherWorker:
     def __init__(self):
+        self.weather_server = 'openweathermap.org'
+        if not self.has_connected():
+            raise ConnectionError
+
         config_dict = get_default_config()
         config_dict['language'] = 'ru'
         self.owm = OWM(API_key, config_dict)
-        self.mgr = self.owm.weather_manager()
+        self.weather_mgr = self.owm.weather_manager()
+        self.city = geocoder.ip('me')
 
-    def one_call_at_coords(self, lat, lon):
-        return self.mgr.one_call()
+    def has_connected(self) -> bool:
+        return bool(socket.create_connection((self.weather_server, 80)))
 
-    def coords_from_place_name(self, place_name):
-        reg = self.owm.city_id_registry()
-        list_of_locations = reg.locations_for(place_name)
-        return list_of_locations
+    # def weather_by_days(self, city: str) -> pyowm.weatherapi25.one_call.OneCall:
+    #     loc = geocoder.location(city)
+    #     return self.weather_mgr.one_call(lat=loc.latitude, lon=loc.longitude)
+
+    # def get_weather_icon(self) -> str:
+    #     return f"{self.weather_server}/img/w/{wt['weather']['weather_icon_name']}.png"
+
+    def weather(self, forecast_type, has_connection):
+        if forecast_type == 'today':
+            self.weather_today(has_connection)
+        elif forecast_type == 'days':
+            self.weather_by_days(has_connection)
+        else:
+            self.weather_by_hours(has_connection)
+
+    def weather_today(self, has_connection):
+        if has_connection:
+            return self.weather_mgr.weather_at_place(self.city.city)
+        else:
+            pass
+
+    def weather_by_days(self, has_connection):
+        if has_connection:
+            loc = geocoder.location(self.city.latlng)
+            return self.weather_mgr.one_call(lat=loc.latitude, lon=loc.longitude).forecast_daily
+        else:
+            pass
+
+    def weather_by_hours(self, has_connection):
+        if has_connection:
+            loc = geocoder.location(self.city.latlng)
+            return self.weather_mgr.one_call(lat=loc.latitude, lon=loc.longitude).forecast_hourly
+        else:
+            pass
+
+    def check_city(self, city):
+        try:
+            self.weather_mgr.weather_at_place(city)
+            return True
+        except NotFoundError:
+            return False
+
+    def change_city(self, city):
+        if self.check_city(city):
+            self.city = city
+            return True
+        return False
 
 
 if __name__ == '__main__':
-    w = Weather()
-    print(w.owm.city_id_registry().locations_for('Москва'))
+    w = WeatherWorker()
